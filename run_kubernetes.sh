3097d2dc96815b881048fd2bcf94a3a275e44bd3
martinkregar <martinkregar@gmail.com>
2020-05-28 09:12:50 +0000

    added run-kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,16 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=martinkregar/udacity-project
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run --image=$dockerpath sklearn
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward pod/sklearn  8080:80
