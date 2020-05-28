7c79681527943143b35fc31448fb136b2ee3ec78
martinkregar <martinkregar@gmail.com>
2020-05-28 06:48:37 +0000

    added logging, docker output, upload docker, fixed run docker script

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=martinkregar/udacity-project
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username martinkregar
+docker tag sklearn $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
