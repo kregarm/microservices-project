7e910b2269d630e19777519c7c19b7368b3822af
studio <studio@Martins-MBP.lan>
2020-05-29 13:53:57 +0200

    fixed run_kubernetes, added kubernetes out

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -9,7 +9,7 @@ dockerpath=martinkregar/udacity-project
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run --image=$dockerpath sklearn
+kubectl run --image=$dockerpath skleran
 
 # Step 3:
 # List kubernetes pods
@@ -17,4 +17,4 @@ kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward pod/sklearn  8080:80
+kubectl port-forward deployment/skleran 8000:80
